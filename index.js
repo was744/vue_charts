ca9ff0fea89367f622c12768c73be11d9e844b6a
was744 <wsanders218@gmail.com>
2019-12-14 20:01:29 -0500

    Init card-deck for charts, init pie chart, TODO: fix chart data

--- index.js
+++ index.js
@@ -42,13 +42,37 @@ var vm3 = new Vue({
 })
 
 var vm4 = new Vue({
-  el: '.card'
+  el: '.chart'
 })
 
 var ctx = document.getElementById('testLine').getContext('2d');
-var chart = new Chart(ctx, {
+var chart1 = new Chart(ctx, {
     // The type of chart we want to create
     type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Crackers Consumed',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+      tooltips: {
+        mode: 'point'
+    }
+    }
+});
+
+var ctx = document.getElementById('testPie').getContext('2d');
+var chart2 = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'pie',
 
     // The data for our dataset
     data: {
